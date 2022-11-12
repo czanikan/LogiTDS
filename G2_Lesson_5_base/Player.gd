33b2a83c70d08378ce4ce32de7c71db55d64a115
Czanik András Béla <32414135+czanikan@users.noreply.github.com>
2022-11-12 11:20:28 +0100

    update

--- G2_Lesson_5_base/Player.gd
+++ G2_Lesson_5_base/Player.gd
@@ -19,3 +19,42 @@ func _ready():
 	set_process(true)
 	bullet = load("res://Scenes/Bullet.tscn")
 	old_playerPos = global_position
+	
+	# make label
+	health_label = Label.new()
+	health_label.text = "HP: " + str(player_health)
+	get_parent().call_deferred("add_child", health_label)
+	
+	ammo_label = Label.new()
+	ammo_label.text = "Ammo: " + str(ammo)
+	get_parent().call_deferred("add_child", ammo_label)
+	
+func _process(delta):
+	health_label.set_global_position(position + Vector2(-40, -45))
+	ammo_label.set_global_position(position + Vector2(-400, 250))
+	
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("DOWN"):
+		velocity.y += speed
+	if Input.is_action_pressed("UP"):
+		velocity.y -= speed
+	if Input.is_action_pressed("RIGHT"):
+		velocity.x += speed
+	if Input.is_action_pressed("LEFT"):
+		velocity.x -= speed
+		
+	if Input.is_action_just_pressed("SHOOT"):
+		if ammo > 0:
+			var b = bullet.instance()
+			b.dir = rotation
+			b.rotation = rotation
+			b.global_position = firepoint.global_position
+			get_parent().add_child(b)
+			ammo -= 1
+			ammo_label.text = "Ammo: " + str(ammo)
+			
+	velocity = move_and_slide(velocity.normalized() * speed)
+	look_at(get_global_mouse_position())
+	
+	new_playerPos = global_position - old_playerPos
+	old_playerPos = global_position
